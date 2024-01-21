2d76b8fe6977ee3622b4b726257e5bcdfdfd1a0d
Kim Kudsk <kim.kudsk@gmail.com>
2024-01-21 20:29:43 +0100

    First atempt, not scrssefull

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
+#include <math.h>
+#include <stdlib.h>
+
 #include "../lib/raylib.h"
-#include <stdio.h>
+
+#define NOB_IMPLEMENTATION
+#include "nob.h"
 
 #define MIN_X -20.0
 #define MAX_X 20.0
@@ -18,17 +23,41 @@ Vector2 project_sample_to_screen(Vector2 sample) {
   };
 }
 
+typedef struct {
+  Vector2 *items;
+  size_t count;
+  size_t capacity;
+} Samples;
+
+float rand_float(void) { return (float)rand() / RAND_MAX; }
+
+void generate_cluster(Vector2 center, float radius, size_t count,
+                      Samples *samples) {
+  for (size_t i = 0; i < count; ++i) {
+    float angle = rand_float() * 2 * PI;
+    float mag = rand_float();
+    Vector2 sample = {
+        .x = center.x * cosf(angle) * mag * radius,
+        .y = center.y * sinf(angle) * mag * radius,
+    };
+    nob_da_append(samples, sample);
+  }
+}
+
 int main(void) {
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(800, 600, "k-means");
+  Samples cluster = {0};
+  generate_cluster(CLITERAL(Vector2){0}, 10, 10, &cluster);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(GetColor(MYBACKGROUNDCOLOR));
-    Vector2 sample = {-10, -10};
-    DrawCircleV(project_sample_to_screen(sample), 10, RED);
-    DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 10, RED);
+    for (size_t i = 0; i < cluster.count; ++i) {
+      Vector2 it = cluster.items[i];
+      DrawCircleV(project_sample_to_screen(it), 10, RED);
+    }
     EndDrawing();
   }
 
