f775f427c1235695024040c9523dcb6711ce7aa5
Kim Kudsk <kim.kudsk@gmail.com>
2024-01-23 23:21:47 +0100

    Reading cvs file

--- src/main.c
+++ src/main.c
@@ -13,20 +13,17 @@
 #define SAMPLE_RADIUS 4.0f
 #define MEAN_RADIUS (SAMPLE_RADIUS * 2)
 #define K 3
-#define MIN_X -20.0
-#define MAX_X 20.0
-#define MIN_Y -20.0
-#define MAX_Y 20.0
 
 #define MYBACKGROUNDCOLOR 0x181818AA
 
-static Vector2 project_sample_to_screen(Vector2 sample) {
+static Vector2 project_sample_to_screen(Vector2 sample, float min_x,
+                                        float max_x, float min_y, float max_y) {
   // -20..20 => 0..40 => 0..1
   float w = GetScreenWidth();
   float h = GetScreenHeight();
   return CLITERAL(Vector2){
-      .x = (sample.x - MIN_X) / (MAX_X - MIN_X) * w,
-      .y = h - (sample.y - MIN_Y) / (MAX_Y - MIN_Y) * h,
+      .x = (sample.x - min_x) / (max_x - min_x) * w,
+      .y = h - (sample.y - min_y) / (max_y - min_y) * h,
   };
 }
 
@@ -61,17 +58,19 @@ static Samples set = {0};
 static Samples clusters[K] = {0};
 static Vector2 means[K] = {0};
 
-void generate_new_state(void) {
+void generate_new_state(float min_x, float max_x, float min_y, float max_y) {
   // Generate the Samples data
+#if 0
   set.count = 0;
   generate_cluster(CLITERAL(Vector2){0}, 10, 100, &set);
-  generate_cluster(CLITERAL(Vector2){MIN_X * 0.5f, MAX_Y * 0.5f}, 5, 50, &set);
-  generate_cluster(CLITERAL(Vector2){MAX_X * 0.5f, MAX_Y * 0.5f}, 5, 50, &set);
+  generate_cluster(CLITERAL(Vector2){min_x * 0.5f, max_y * 0.5f}, 5, 50, &set);
+  generate_cluster(CLITERAL(Vector2){max_x * 0.5f, max_y * 0.5f}, 5, 50, &set);
+#endif
 
   // Calculation of the K-means
   for (size_t i = 0; i < K; ++i) {
-    means[i].x = Lerp(MIN_X, MAX_X, rand_float());
-    means[i].y = Lerp(MIN_Y, MAX_Y, rand_float());
+    means[i].x = Lerp(min_x, max_x, rand_float());
+    means[i].y = Lerp(min_y, max_y, rand_float());
   }
 }
 
@@ -95,7 +94,7 @@ void recluster_state(void) {
   }
 }
 
-void update_means(void) {
+void update_means(float min_x, float max_x, float min_y, float max_y) {
   for (size_t i = 0; i < K; ++i) {
     if (clusters[i].count > 0) {
       means[i] = Vector2Zero();
@@ -105,27 +104,91 @@ void update_means(void) {
       means[i].x /= clusters[i].count;
       means[i].y /= clusters[i].count;
     } else {
-      means[i].x = Lerp(MIN_X, MAX_X, rand_float());
-      means[i].y = Lerp(MIN_Y, MAX_Y, rand_float());
+      means[i].x = Lerp(min_x, max_x, rand_float());
+      means[i].y = Lerp(min_y, max_y, rand_float());
     }
   }
 }
 
+typedef enum {
+  LEAF_CLASS = 0,
+  LEAF_SPECIMEN_NUMBER,
+  LEAF_ECCENTRICITY,
+  LEAF_ASPECT_RATIO,
+  LEAF_ELONGATION,
+  LEAF_SOLIDITY,
+  LEAF_STOCHASTIC_CONVEXITY,
+  LEAF_ISOPERIMETRIC_FACTOR,
+  LEAF_MAXIMAL_INDENTATION_DEPTH,
+  LEAF_LOBEDNESS,
+  LEAF_AVERAGE_INTENSITY,
+  LEAF_AVERAGE_CONTRAST,
+  LEAF_SMOOTHNESS,
+  LEAF_THIRD_MOMENT,
+  LEAF_UNIFORMITY,
+  LEAF_ENTROPY,
+} Leaf_Attr;
+
 int main(void) {
+  const char *leaf_path = "data/leaf.csv";
+  Nob_String_Builder sb = {0};
+  if (!nob_read_entire_file(leaf_path, &sb))
+    return 1;
+
+  float min_x = FLT_MAX;
+  float max_x = FLT_MIN;
+  float min_y = FLT_MAX;
+  float max_y = FLT_MIN;
+
+  Nob_String_View content = nob_sv_from_parts(sb.items, sb.count);
+  while (content.count > 0) {
+    Nob_String_View line = nob_sv_chop_by_delim(&content, '\n');
+    Vector2 p = {0};
+    for (size_t i = 0; line.count > 0; ++i) {
+      Nob_String_View attr = nob_sv_chop_by_delim(&line, ',');
+      float value = strtof(nob_temp_sprintf(SV_Fmt, SV_Arg(attr)), NULL);
+      switch (i) {
+      case LEAF_CLASS:
+        p.x = value;
+        break;
+      case LEAF_ENTROPY:
+        p.y = value;
+        break;
+      default: {
+      }
+      }
+    }
+    nob_da_append(&set, p);
+    if (p.x < min_x)
+      min_x = p.x;
+    if (p.x > max_x)
+      max_x = p.x;
+    if (p.y < min_y)
+      min_y = p.y;
+    if (p.y > max_y)
+      max_y = p.y;
+  }
+  nob_temp_reset();
+
+  for (size_t i = 0; i < set.count; ++i) {
+    Vector2 p = set.items[i];
+    nob_log(NOB_INFO, "(%f, %f)", p.x, p.y);
+  }
+
   srand(time(0));
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(800, 600, "K-means");
 
-  generate_new_state();
+  generate_new_state(min_x, max_x, min_y, max_y);
   recluster_state();
 
   while (!WindowShouldClose()) {
     if (IsKeyPressed(KEY_R)) {
-      generate_new_state();
+      generate_new_state(min_x, max_x, min_y, max_y);
       recluster_state();
     }
     if (IsKeyPressed(KEY_SPACE)) {
-      update_means();
+      update_means(min_x, max_x, min_y, max_y);
       recluster_state();
     }
     BeginDrawing();
@@ -136,11 +199,13 @@ int main(void) {
 
       for (size_t j = 0; j < clusters[i].count; ++j) {
         Vector2 it = clusters[i].items[j];
-        DrawCircleV(project_sample_to_screen(it), SAMPLE_RADIUS, color);
+        DrawCircleV(project_sample_to_screen(it, min_x, max_x, min_y, max_y),
+                    SAMPLE_RADIUS, color);
       }
 
       Vector2 it = means[i];
-      DrawCircleV(project_sample_to_screen(it), MEAN_RADIUS, color);
+      DrawCircleV(project_sample_to_screen(it, min_x, max_x, min_y, max_y),
+                  MEAN_RADIUS, color);
     }
     EndDrawing();
   }
