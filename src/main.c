afcf8fac756351bbaa231d9cf6454d5f64c7fcbe
Kim Kudsk <kim.kudsk@gmail.com>
2024-01-23 22:02:26 +0100

    Update Means

--- src/main.c
+++ src/main.c
@@ -95,6 +95,22 @@ void recluster_state(void) {
   }
 }
 
+void update_means(void) {
+  for (size_t i = 0; i < K; ++i) {
+    if (clusters[i].count > 0) {
+      means[i] = Vector2Zero();
+      for (size_t j = 0; j < clusters[i].count; ++j) {
+        means[i] = Vector2Add(means[i], clusters[i].items[j]);
+      }
+      means[i].x /= clusters[i].count;
+      means[i].y /= clusters[i].count;
+    } else {
+      means[i].x = Lerp(MIN_X, MAX_X, rand_float());
+      means[i].y = Lerp(MIN_Y, MAX_Y, rand_float());
+    }
+  }
+}
+
 int main(void) {
   srand(time(0));
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
@@ -106,6 +122,10 @@ int main(void) {
   while (!WindowShouldClose()) {
     if (IsKeyPressed(KEY_R)) {
       generate_new_state();
+      recluster_state();
+    }
+    if (IsKeyPressed(KEY_SPACE)) {
+      update_means();
       recluster_state();
     }
     BeginDrawing();
