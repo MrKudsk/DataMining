9bfce35424d7cff3ec8983a2d60e83a5b4edd817
Kim Kudsk <kim.kudsk@gmail.com>
2024-01-26 19:56:30 +0100

    Read file

--- src/knn.c
+++ src/knn.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
 int main(void) {
-  printf("Hello, World");
+  const char *file_path = "./data/Ag-News/train.csv";
+  Nob_String_Builder content = {0};
+  if (!nob_read_entire_file(file_path, &content)) {
+    return 1;
+  }
+
+  nob_log(NOB_INFO, "Size of %s is %zu bytes", file_path, content.count);
+
   return 0;
 }
